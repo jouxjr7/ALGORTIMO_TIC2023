9245517837bbea527eefcbdcecc9db7649455631
jouxjr7 <81985586+jouxjr7@users.noreply.github.com>
2023-09-19 21:55:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import requests
+import json
+import datetime
 from typing import Union
 from disponibilityFunctions import processDisponibility
 from aula1Functions import process, obtenerHorarios
@@ -9,28 +12,43 @@ from aula3Functions import process3
 
 
 app = FastAPI()
-# funcion que habilita comunicación con la app web accediendo a las politicas de cors
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# ruta para encontrar disponibilidad del aula 
 @app.get("/disponibility")
 async def get_aulas( idSchedule:int,dayTest:int, piso:str,  buildCode:str, morning:bool  ):
     listaPython = obtenerHorarios(idSchedule)
     respApi=processDisponibility(listaPython,piso,buildCode,dayTest, morning)
     return(respApi)
-        
-# ruta para encontrar aula con una sesion 
+
+
+@app.get("/prueba")
+async def get_aulas( idSchedule:int):
+    peticion  = f'http://localhost:7130/api/classroom/byCalendar/{idSchedule}'
+    response = requests.get(peticion, verify=False)
+    listaJson =  response.json()
+    cadenaJson = json.dumps(listaJson)
+    listaPython = json.loads(cadenaJson)
+    return listaPython
+
+@app.get("/")
+async def get_aulas( ):
+     
+    return ("API CORRIENDO")
+    
+            
+
 @app.get("/aulas1")
 async def get_aulas(idSchedule:int,isLab:bool,startTimeTest:str, endTimeTest:str, dayTest:int, 
                     piso:Union[str, None] = None, capacity:Union[str, None] = None, buildCode:Union[str, None] = None,nameLab:Union[str, None] = None):
     listaPython = obtenerHorarios(idSchedule)
     respuestaApi=process(listaPython,isLab,nameLab,startTimeTest, endTimeTest, dayTest, piso, capacity, buildCode)
     return(respuestaApi)
-# ruta para encontrar aula con dos sesiones 
+
 @app.get("/aulas2")
 async def get_aulas(idSchedule:int,isLab:bool,startTimeTest:str, endTimeTest:str, dayTest:int,
                     startTimeTest2:str, endTimeTest2:str, dayTest2:int,
@@ -39,7 +57,7 @@ async def get_aulas(idSchedule:int,isLab:bool,startTimeTest:str, endTimeTest:str
     respuestaApi=process2(listaPython,isLab,nameLab,startTimeTest, endTimeTest,
                          dayTest, piso, capacity, buildCode,startTimeTest2, endTimeTest2, dayTest2)
     return(respuestaApi) 
-# ruta para encontrar aula con tres sesiones 
+
 @app.get("/aulas3")
 async def get_aulas(idSchedule:int,isLab:bool,startTimeTest:str, endTimeTest:str, dayTest:int,
                     startTimeTest2:str, endTimeTest2:str, dayTest2:int,
@@ -52,6 +70,5 @@ async def get_aulas(idSchedule:int,isLab:bool,startTimeTest:str, endTimeTest:str
                          startTimeTest3, endTimeTest3, dayTest3)
     return(respuestaApi) 
 
-# definicion del puerto y direccion del servidor que la api va a ofertar el servicio
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
